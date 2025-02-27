165b6ecaed2ce9fee6dfeeae3a4ed21cbb0e8199
sionkim0126 <kimsion000@naver.com>
2025-02-27 17:13:27 +0900

    c언어Test 및 기본설정

---
+++ c_code/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    // 사용자로부터 두 정수 A와 B를 입력받음
+    scanf("%d %d", &A, &B);
+    // 입력받은 두 정수의 합을 출력
+    printf("%d\n", A + B);
+    return 0;
+    
+}
