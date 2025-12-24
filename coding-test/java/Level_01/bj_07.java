93356c44add39609e2d0bbd7e1453328ab0ecedb
sionkim0126 <kimsion000@naver.com>
2025-12-24 23:48:03 +0900

    feat : 사칙연산 네가지 식 출력 문제

---
+++ coding-test/java/Level_01/bj_07.java
@@ -0,0 +1,33 @@
+/*
+문제 : (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+       (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+       세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+       첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+번호 : 10430
+조건 : 첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+출처 : BaekJoon
+*/
+
+import java.util.Scanner;
+
+public class bj_07 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(2 <= a && a <= 10000){
+            if(2 <= b && b <= 10000){
+                if(2 <= c && c <= 10000){
+                    System.out.println((a+b)%c);
+                    System.out.println(((a % c) + (b % c)) % c);
+                    System.out.println((a*b)%c);
+                    System.out.println(((a%c)*(b%c))%c);
+                }
+            }
+        }
+
+    }
+}
