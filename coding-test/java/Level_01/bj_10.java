694cf608989cdd727cbbd51d9ef317093524f1ba
sionkim0126 <kimsion000@naver.com>
2025-12-28 00:11:49 +0900

    practice: 이스케이프 문자 출력 문제 해결

---
+++ coding-test/java/Level_01/bj_10.java
@@ -0,0 +1,14 @@
+/* 
+문제 : 고양이를 출력하시오.
+번호 : 10171
+조건 : x
+출처 : BaekJoon
+*/
+public class bj_10 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }    
+}
---
+++ coding-test/java/Level_01/bj_11.java
@@ -0,0 +1,16 @@
+/* 
+문제 : 개를 출력하시오.
+번호 : 10172
+조건 : x
+출처 : BaekJoon
+*/
+public class bj_11 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
