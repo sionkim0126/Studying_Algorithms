85879d59bcd88eba99198844ca645364d79c8c90
sionkim0126 <kimsion000@naver.com>
2025-12-30 10:19:02 +0900

    practice : 2단계 두 수의 대소 비교 문제

--- coding-test/java/Level_02/bj02_01.java
+++ coding-test/java/Level_02/bj02_01.java
@@ -11,7 +11,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class bj_02_01 {
+public class bj02_01 {
     public static void main(String[] args) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
