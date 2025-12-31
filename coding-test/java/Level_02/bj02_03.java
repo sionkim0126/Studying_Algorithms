5f7de62a1390a1cef9c4fb8461392ebdc409b3c6
sionkim0126 <kimsion000@naver.com>
2025-12-31 14:04:38 +0900

    practice : 윤년을 판별하는 문제

---
+++ coding-test/java/Level_02/bj02_03.java
@@ -0,0 +1,31 @@
+/* 
+문제 : 연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.
+번호 : 2753
+조건 : 첫째 줄에 연도가 주어진다. 연도는 1보다 크거나 같고, 4000보다 작거나 같은 자연수이다.
+윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다.
+1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아니다. 하지만, 2000년은 400의 배수이기 때문에 윤년이다.
+출처 : BaekJoon
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj02_03 {
+    public static void main(String[] args) {
+        BufferedReader bu = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            int year = Integer.parseInt(bu.readLine());
+            if(1 <= year && year <= 4000){
+                boolean result = year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+                if( result == true){
+                    System.out.println(1);
+                }else{
+                    System.out.println(0);
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
