ebeec6690ff10ab42775eb6ac46db97e9a221d9f
sionkim0126 <kimsion000@naver.com>
2026-01-22 22:59:12 +0900

    practor : 대량 입력 처리를 위해 빠른 입출력 적용 문제3

---
+++ coding-test/java/Level_03/bj03_08.java
@@ -0,0 +1,26 @@
+/*
+문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+번호 : 15552
+조건 : 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+        각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+        각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+출처 : BaekJoon
+*/
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class bj03_08 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader bu = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(bu.readLine());
+        for(int i = 1; i <= t; i++){
+            String arr[] = bu.readLine().split(" ");
+            int sum = Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]);
+            bw.write("Case #" + i +": " + arr[0] + " + " + arr[1] + " = " + sum + "\n");
+        }
+        bw.flush();
+    }
+}
