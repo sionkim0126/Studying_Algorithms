3ede88911c2433e34ec19c61c5c778b77d0c1594
sionkim0126 <kimsion000@naver.com>
2026-02-16 15:03:54 +0900

    practice : BaekJoon class1 no.25083 새싹을 출력하는 문제

---
+++ coding-test/java/Class_01/bj_25083.java
@@ -0,0 +1,16 @@
+/*
+문제 : 아래 예제와 같이 새싹을 출력하시오.
+번호 : 25083
+조건 : 입력없음.
+출처 : BaekJoon
+*/
+public class bj_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
