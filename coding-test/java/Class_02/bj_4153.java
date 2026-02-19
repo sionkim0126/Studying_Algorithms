38363060f11a8eb3b5dd8d430c55c05e4915ca1e
sionkim0126 <kimsion000@naver.com>
2026-02-19 10:43:30 +0900

    practice : BaekJoon Class2 no.4153 직각삼각형 구분 문제

---
+++ coding-test/java/Class_02/bj_4153.java
@@ -0,0 +1,75 @@
+/*
+문제 : 과거 이집트인들은 각 변들의 길이가 3, 4, 5인 삼각형이 직각 삼각형인것을 알아냈다. 주어진 세변의 길이로 삼각형이 직각인지 아닌지 구분하시오.
+번호 : 4153
+조건 : 입력은 여러개의 테스트케이스로 주어지며 마지막줄에는 0 0 0이 입력된다. 각 테스트케이스는 모두 30,000보다 작은 양의 정수로 주어지며, 각 입력은 변의 길이를 의미한다.
+        각 입력에 대해 직각 삼각형이 맞다면 "right", 아니라면 "wrong"을 출력한다.
+출처 : BaekJoon
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj_4153 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String result = null;
+            while (true) {
+                String[] triangle = br.readLine().split(" ");
+                
+                int a = Integer.parseInt(triangle[0]);
+                int b = Integer.parseInt(triangle[1]);
+                int c = Integer.parseInt(triangle[2]);
+
+                if(a > b && a > c){
+                    if(a*a == (b*b) + (c*c)){
+                        result = "right";
+                    }else{
+                        result = "wrong";
+                    }
+                }else if(b > a && b > c){
+                    if(b*b == (a*a) + (c*c)){
+                        result = "right";
+                    }else{
+                        result = "wrong";
+                    }
+                }else if(c > a && c > a){
+                    if(c*c == (b*b) + (a*a)){
+                        result = "right";
+                    }else{
+                        result = "wrong";
+                    }
+                }else if (a == 0 && b == 0 && c == 0) {
+                    break;
+                }
+                System.out.println(result);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+/* 
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int[] a = new int[3];
+       while(true) {
+           a[0] = sc.nextInt();
+           a[1] = sc.nextInt();
+           a[2] = sc.nextInt();
+           if(a[0] == 0 && a[1] == 0 && a[2] == 0) {
+        	   break;
+           }
+           Arrays.sort(a);
+           if((a[0] * a[0]) + (a[1] * a[1]) == (a[2] * a[2])) {
+        	   System.out.println("right");
+           }else {
+        	   System.out.println("wrong");
+           }    	   
+       }
+    }
+}
+*/
